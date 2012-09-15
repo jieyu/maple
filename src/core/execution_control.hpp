ad8f722be94e227f893af340b2fdd99616167e0d
Jie Yu <yujie.jay@gmail.com>
2012-09-15 04:02:31 -0400

    Refactor to use a macro to replace all the entry functions.

--- src/core/execution_control.hpp
+++ src/core/execution_control.hpp
@@ -19,6 +19,7 @@
 #ifndef CORE_EXECUTION_CONTROL_HPP_
 #define CORE_EXECUTION_CONTROL_HPP_
 
+#include <csignal>
 #include <list>
 #include <map>
 
@@ -74,6 +75,85 @@
 
 #define ACTIVATE_WRAPPER_HANDLER(name)                                      \
   ACTIVATE_WRAPPER(name, img, STATIC_WRAPPER_HANDLER(name));
+
+// Define macros for the main entry functions.
+#define MAIN_ENTRY(controller)                                              \
+  static controller *ctrl = new controller;                                 \
+  static VOID I_InstrumentTrace(TRACE trace, VOID *v) {                     \
+    ctrl->InstrumentTrace(trace, v);                                        \
+  }                                                                         \
+  static VOID I_ImageLoad(IMG img, VOID *v) {                               \
+    ctrl->ImageLoad(img, v);                                                \
+  }                                                                         \
+  static VOID I_ImageUnload(IMG img, VOID *v) {                             \
+    ctrl->ImageUnload(img, v);                                              \
+  }                                                                         \
+  static VOID I_SyscallEntry(THREADID tid,                                  \
+                             CONTEXT *ctxt,                                 \
+                             SYSCALL_STANDARD std,                          \
+                             VOID *v) {                                     \
+    ctrl->SyscallEntry(tid, ctxt, std, v);                                  \
+  }                                                                         \
+  static VOID I_SyscallExit(THREADID tid,                                   \
+                            CONTEXT *ctxt,                                  \
+                            SYSCALL_STANDARD std,                           \
+                            VOID *v) {                                      \
+    ctrl->SyscallExit(tid, ctxt, std, v);                                   \
+  }                                                                         \
+  static BOOL I_IntSignal(THREADID tid,                                     \
+                          INT32 sig,                                        \
+                          CONTEXT *ctxt,                                    \
+                          BOOL hasHandler,                                  \
+                          const EXCEPTION_INFO *pExceptInfo,                \
+                          VOID *v) {                                        \
+    ctrl->IntSignal(tid, sig, ctxt, hasHandler, pExceptInfo, v);            \
+    return FALSE;                                                           \
+  }                                                                         \
+  static VOID I_ContextChange(THREADID tid,                                 \
+                              CONTEXT_CHANGE_REASON reason,                 \
+                              const CONTEXT *from,                          \
+                              CONTEXT *to,                                  \
+                              INT32 info,                                   \
+                              VOID *v) {                                    \
+    ctrl->ContextChange(tid, reason, from, to, info, v);                    \
+  }                                                                         \
+  static VOID I_ProgramStart() {                                            \
+    ctrl->ProgramStart();                                                   \
+  }                                                                         \
+  static VOID I_ProgramExit(INT32 code, VOID *v) {                          \
+    ctrl->ProgramExit(code, v);                                             \
+  }                                                                         \
+  static VOID I_ThreadStart(THREADID tid,                                   \
+                            CONTEXT *ctxt,                                  \
+                            INT32 flags,                                    \
+                            VOID *v) {                                      \
+    ctrl->ThreadStart(tid, ctxt, flags, v);                                 \
+  }                                                                         \
+  static VOID I_ThreadExit(THREADID tid,                                    \
+                           const CONTEXT *ctxt,                             \
+                           INT32 code,                                      \
+                           VOID *v) {                                       \
+    ctrl->ThreadExit(tid, ctxt, code, v);                                   \
+  }                                                                         \
+  int main(int argc, char *argv[]) {                                        \
+    ctrl->Initialize();                                                     \
+    ctrl->PreSetup();                                                       \
+    PIN_InitSymbols();                                                      \
+    PIN_Init(argc, argv);                                                   \
+    ctrl->PostSetup();                                                      \
+    TRACE_AddInstrumentFunction(I_InstrumentTrace, NULL);                   \
+    IMG_AddInstrumentFunction(I_ImageLoad, NULL);                           \
+    IMG_AddUnloadFunction(I_ImageUnload, NULL);                             \
+    PIN_AddSyscallEntryFunction(I_SyscallEntry, NULL);                      \
+    PIN_AddSyscallExitFunction(I_SyscallExit, NULL);                        \
+    PIN_InterceptSignal(SIGUSR2, I_IntSignal, NULL);                        \
+    PIN_AddContextChangeFunction(I_ContextChange, NULL);                    \
+    PIN_AddFiniFunction(I_ProgramExit, NULL);                               \
+    PIN_AddThreadStartFunction(I_ThreadStart, NULL);                        \
+    PIN_AddThreadFiniFunction(I_ThreadExit, NULL);                          \
+    I_ProgramStart();                                                       \
+    PIN_StartProgram();                                                     \
+  }
 
 // The main controller for the dynamic program analysis.
 class ExecutionControl {

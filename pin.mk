fd7d4fb8aebf26ab07dc61e2402c541ce413d555
Jie Yu <yujie.jay@gmail.com>
2014-01-12 19:39:32 -0500

    Allowed Maple to work with the latest PIN kit.

--- pin.mk
+++ pin.mk
@@ -4,25 +4,8 @@ ifndef PIN_HOME
   $(error Please define PIN_HOME environment.)
 endif
 
-PIN_KIT = $(PIN_HOME)
-KIT = 1
+PIN_ROOT := $(PIN_HOME)
 
-TARGET_COMPILER ?= gnu
-ifdef OS
-  ifeq (${OS}, Windows_NT)
-    TARGET_COMPILER = ms
-  endif
-endif
-
-ifeq ($(TARGET_COMPILER), gnu)
-  include $(PIN_HOME)/source/tools/makefile.gnu.config
-  CXXFLAGS ?= -Wall -Werror -Wno-unknown-pragmas $(DBG) $(OPT)
-  PIN = $(PIN_HOME)/pin
-endif
-
-ifeq ($(TARGET_COMPILER), ms)
-  include $(PIN_HOME)/source/tools/makefile.ms.config
-  DBG ?=
-  PIN = $(PIN_HOME)/pin.bat
-endif
-
+CONFIG_ROOT := $(PIN_ROOT)/source/tools/Config
+include $(CONFIG_ROOT)/makefile.config
+#include $(TOOLS_ROOT)/Config/makefile.default.rules

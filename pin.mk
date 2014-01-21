d0ca843c41944e27c5eaa42b3d387e3b210563de
Jie Yu <yujie.jay@gmail.com>
2014-01-20 19:37:49 -0500

    Enabled Maple to build with PinPlay.

--- pin.mk
+++ pin.mk
@@ -9,3 +9,15 @@ PIN_ROOT := $(PIN_HOME)
 CONFIG_ROOT := $(PIN_ROOT)/source/tools/Config
 include $(CONFIG_ROOT)/makefile.config
 #include $(TOOLS_ROOT)/Config/makefile.default.rules
+
+# PinPlay build.
+ifeq ($(pinplay), 1)
+  PINPLAY_HOME := $(PIN_HOME)/extras/pinplay
+  CFLAGS += -DCONFIG_PINPLAY
+  CXXFLAGS += -DCONFIG_PINPLAY
+  INCS += -I$(PINPLAY_HOME)/include
+  INCS += -I$(PINPLAY_HOME)/include-ext
+  TOOL_LPATHS += -L$(PINPLAY_HOME)/lib/$(TARGET)
+  TOOL_LPATHS += -L$(PINPLAY_HOME)/lib-ext/$(TARGET)
+  TOOL_LIBS += -lpinplay -lbz2 -lz
+endif
